78b44cb8f11973d113331ef9bbb00742cd1c53d0
Wiseman11 <manwise047@gmail.com>
2023-08-16 12:14:49 +0300

    faqs

--- main.js
+++ main.js
@@ -4,16 +4,23 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
 
+// show/hide faq answer
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
 
 
-
-
-
-
-
-
-
-
+        // change icon
+        const icon = faq.querySelector('.faq__icon ion-icon');
+        if (icon.getAttribute("name") === 'chevron-down-outline') {
+            icon.setAttribute("name", "chevron-up-outline");
+        } else{
+            icon.setAttribute("name", "chevron-down-outline");
+        }
+    })
+})
 
 
 // show/hide nav menu
