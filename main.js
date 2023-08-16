872db331058f00a18a4703f08a2415d99f1640e2
Wiseman11 <manwise047@gmail.com>
2023-08-16 17:02:04 +0300

    filters

--- main.js
+++ main.js
@@ -23,6 +23,40 @@ faqs.forEach(faq => {
 })
 
 
+
+// filters
+document.addEventListener("DOMContentLoaded", function() {
+    const filterButtons = document.querySelectorAll(".filter-button");
+    const items = document.querySelectorAll(".service__member");
+
+    filterButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            filterButtons.forEach(btn => btn.classList.remove("active"));
+            this.classList.add("active");
+
+            const filterValue = this.getAttribute("data-filter");
+
+            items.forEach(item => {
+                item.classList.remove("filtered");
+                if (filterValue !== "all" && !item.classList.contains(filterValue)) {
+                    item.classList.add("filtered");
+                }
+            })
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
 // show/hide nav menu
 const menu = document.querySelector(".nav__menu");
 const menuBtn = document.querySelector("#open-menu-btn");
